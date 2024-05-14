1c4a07407467a14d2ff163e06bd48fd1db88cb6d
Fidelis Adang <fidenki1429@gmail.com>
2024-05-14 11:07:59 +0100

    implement navbar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,38 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSIdebar from '@/components/RightSIdebar'
+import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  return <div>Home</div>
+  const loggedIn = {
+    firstName: 'JustFidel',
+    lastName: 'Adang',
+    email: 'justfidel2022@gmail.com',
+  }
+
+  return (
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type='greeting'
+            title='welcome'
+            user={loggedIn?.firstName || 'Guest'}
+            subtext='Access and manage your investments efficiently.'
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={10000}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSIdebar user={loggedIn} transactions={[]} banks={[{}, {}]} />
+    </section>
+  )
 }
 
 export default Home
